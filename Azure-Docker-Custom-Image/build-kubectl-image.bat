ec3871328855d62b77fced2f830210b7bd733ce7
hapheej-shekh <shapheej@gmail.com>
2025-10-05 00:44:56 +0530

    docker update

--- Azure-Docker-Custom-Image/build-kubectl-image.bat
+++ Azure-Docker-Custom-Image/build-kubectl-image.bat
@@ -5,8 +5,21 @@ curl -L -s https://dl.k8s.io/release/stable.txt > version.txt
 REM Read the version into a variable
 set /p KUBECTL_VERSION=<version.txt
 
+REM Can hardcode version as well as below
+REM set KUBECTL_VERSION=v1.29.4
+
 echo Building Docker image with kubectl version %KUBECTL_VERSION%
 docker build --build-arg KUBECTL_VERSION=%KUBECTL_VERSION% -t azure-cli-kubectl .
 
 REM Cleanup
 del version.txt
+
+REM Run the container interactively
+echo Running container from image azure-cli-kubectl...
+
+REM Below command have less arguments
+REM docker run -it --rm azure-cli-kubectl
+
+REM Below command will not start jenkins server
+docker run -it --rm -v %USERPROFILE%\.kube:/root/.kube azure-cli-kubectl
+
--- Jenkinsfile
+++ Jenkinsfile
@@ -9,7 +9,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/hapheej-shekh/spring-cicd-docker-jenkins.git'
+                git branch: 'main', url: 'https://github.com/hapheej-shekh/spring-cicd-jenkins-azure.git'
             }
         }
 
